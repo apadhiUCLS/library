8629832fc6dcc27fd65b3ac3bb2ae7d40f9319b3
acifu <acifu@ucls.uchicago.edu>
2022-05-06 10:10:26 -0500

    finished person class

--- src/main/java/com/example/library/Person.java
+++ src/main/java/com/example/library/Person.java
@@ -4,17 +4,20 @@ import java.util.*;
 public class Person {
     private ArrayList<Book>  checkedOutBooks;
     private ArrayList<Book>  heldBooks;
+    private ArrayList<Book>  overdue;
     private String name;
 
     public Person(String name){
         this.checkedOutBooks = new ArrayList<Book>();
         this.heldBooks = new ArrayList<Book>();
+        this.overdue = new ArrayList<Book>();
         this.name = name;
     }
 
     public Person(){
         this.checkedOutBooks = new ArrayList<Book>();
         this.heldBooks = new ArrayList<Book>();
+        this.overdue = new ArrayList<Book>();
         this.name = "anonymous";
     }
 
@@ -34,8 +37,36 @@ public class Person {
         this.heldBooks.remove(index);
     }
 
+    public boolean inHolds(Book book) {
+        if (this.heldBooks.indexOf(book) < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public ArrayList<Book> getHolds() {return heldBooks;}
+    public ArrayList<Book> getCheckedOut() {return checkedOutBooks;}
+    public ArrayList<Book> overdue() {return overdue;}
+
     public void returnBook(Book book) {
-        int index = this.checkedOutBooks.indexOf(book);
-        this.checkedOutBooks.remove(index);
+        if (this.checkedOutBooks.indexOf(book) < 0) {
+            this.checkedOutBooks.remove(checkedOutBooks.indexOf(book));
+        }
+        if (this.overdue.indexOf(book) < 0) {
+            this.overdue.remove(overdue.indexOf(book));
+        }
+    }
+
+    public boolean checkOverdue() {
+        boolean existsOverdue = false;
+        Date today = new Date();
+        for (int i = 0; i < checkedOutBooks.size(); i++) {
+            if (checkedOutBooks.get(i).getReleaseDate().compareTo(today) > 0) {
+                existsOverdue = true;
+                this.overdue.add(checkedOutBooks.get(i));
+            }
+        }
+        return existsOverdue;
     }
 }

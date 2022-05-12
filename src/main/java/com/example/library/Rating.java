9e6d8bf5f740de6f9005f68370ec396f41d144cb
apadhiUCLS <apadhi@ucls.uchicago.edu>
2022-05-12 08:06:47 -0500

    rating class/constructor created

---
+++ src/main/java/com/example/library/Rating.java
@@ -0,0 +1,10 @@
+package com.example.library;
+
+public class Rating {
+    private String words;
+    private int stars;
+    public Rating(String s, int i){
+        words=s;
+        stars=i;
+    }
+}

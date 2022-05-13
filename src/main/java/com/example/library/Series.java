3b65a4341f9d194bcd514642a0063a64d3cc81e6
apadhiUCLS <apadhi@ucls.uchicago.edu>
2022-05-13 07:02:51 -0500

    added series instance variables/constructors

--- src/main/java/com/example/library/Series.java
+++ src/main/java/com/example/library/Series.java
@@ -1,4 +1,12 @@
 package com.example.library;
 
+import java.util.ArrayList;
+
 public class Series {
+    private String title;
+    private ArrayList<Book> books;
+    public series(String title, ArrayList<Book> books){
+        this.title=title;
+        this.books=books;
+    }
 }
